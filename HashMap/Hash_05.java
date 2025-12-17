ad1b9035b299c3e541853dd13eb47193de163efe
Pruthviraj <pruthvirajthorbole96@gmail.com>
2025-12-17 19:38:31 +0530

    added a hashmap questions

---
+++ HashMap/Hash_05.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Hash_05 {
+    public static void main(String[] args) {
+
+        HashMap<String,Integer> map = new HashMap<>();
+
+        map.put("sahil",5);
+        map.put("john",25);
+        map.put("lukas",63);
+        map.put("Vaishu",26);
+        map.put("lukas",5);
+
+
+        // for(Map.Entry<String,Integer> entry:map.entrySet()){
+        //     System.out.println(entry.getKey()+"->"+entry.getValue());
+
+        // }
+
+        System.out.println(map);
+
+
+
+
+
+
+    }
+    
+}
